65f38e0c573534bb94b785e09da1eb8ab4be4953
Nacho <ignaciop000@gmail.com>
2019-02-05 09:39:09 -0300

    Update modulation.py

--- modulation.py
+++ modulation.py
@@ -14,8 +14,11 @@ def ask():
     pulse = 0.5*signal.square(2*math.pi*freq_pulse*time)+0.5
     ask = carrier * pulse
     plt.title("ASK")
+    plt.subplot(3,1,1)
     plt.plot(time, carrier)
+    plt.subplot(3,1,2)
     plt.plot(time, pulse)
+    plt.subplot(3,1,3)
     plt.plot(time, ask)
     plt.show()
 
@@ -26,15 +29,58 @@ def fsk():
     freq_pulse = 5
     amplitude = 3
     time = np.arange(0, 1, 0.001)
-    carrier = amplitude*np.sin(2*math.pi*freq_carrier*time)
+    carrier_freq_1 = amplitude*np.sin(2*math.pi*(freq_carrier+freq_delta)*time)
+    carrier_freq_2 = amplitude*np.sin(2*math.pi*(freq_carrier-freq_delta)*time)
     pulse = 0.5*signal.square(2*math.pi*freq_pulse*time)+0.5
-    ask = carrier * pulse
+    fsk = []
+    for i in range(len(time)):
+        if pulse[i] == 0:
+            fsk.append (carrier_freq_2[i])
+        else:
+            fsk.append(carrier_freq_1[i])
     plt.title("FSK")
-    plt.plot(time, carrier)
+    plt.subplot(4,1,1)
+    plt.plot(time, carrier_freq_1)
+    plt.subplot(4,1,2)
+    plt.plot(time, carrier_freq_2)
+    plt.subplot(4,1,3)
     plt.plot(time, pulse)
-    plt.plot(time, ask)
+    plt.subplot(4,1,4)
+    plt.plot(time, fsk)
+    plt.show()
+
+def bpsk():
+    #dos fases BPSK o PAM
+    #cuatro fases QPSK o QAM
+    #ocho fases 8-PSK y asi
+    print 'Modulation FSK'
+    freq_carrier = 25
+    fase1 = 0
+    fase2 = 180
+    freq_pulse = 5
+    amplitude = 3
+    time = np.arange(0, 1, 0.001)
+    carrier_fase_1 = amplitude*np.cos(2*math.pi*freq_carrier*time+fase1)
+    carrier_fase_2 = amplitude*np.cos(2*math.pi*freq_carrier*time+fase2)
+    pulse = 0.5*signal.square(2*math.pi*freq_pulse*time)+0.5
+    fsk = []
+    for i in range(len(time)):
+        if pulse[i] == 0:
+            fsk.append (carrier_fase_2[i])
+        else:
+            fsk.append(carrier_fase_1[i])
+    plt.title("BPSK")
+    plt.subplot(4,1,1)
+    plt.plot(time, carrier_fase_1)
+    plt.subplot(4,1,2)
+    plt.plot(time, carrier_fase_2)
+    plt.subplot(4,1,3)
+    plt.plot(time, pulse)
+    plt.subplot(4,1,4)
+    plt.plot(time, fsk)
     plt.show()
 
 if __name__ == "__main__":	    
     #ask()
-    fsk()
+    #fsk()
+    bpsk()
